cd3f21c60952876aee24a25603d63b3421d0fac7
Vadym Tyemirov <temirov@gmail.com>
2015-12-15 10:58:51 -0800

    Minor restyling

--- example/blocking.rb
+++ example/blocking.rb
@@ -2,7 +2,7 @@ require 'redis_reliable_queue'
 require 'thread'
 
 redis = Redis.new
-#Create a queue that will listen for a new element for 10 seconds
+# Create a queue that will listen for a new element for 10 seconds
 queue = Redis::ReliableQueue.new(redis: redis, timeout: 10)
 queue.clear true
 
@@ -11,14 +11,14 @@ queue.clear true
 # Simulate a delayed insert
 t = Thread.new do
   sleep 3
-   # We should use a second connection here since the first one is busy
-   # on a blocking call
-  _redis = Redis.new
-  _queue = Redis::ReliableQueue.new(redis: _redis)
-  100.times { _queue << "e_#{rand(100)}" }
+  # We should use a second connection here since the first one is busy
+  # on a blocking call
+  redis_in_thread = Redis.new
+  queue_in_thread = Redis::ReliableQueue.new(redis: redis_in_thread)
+  100.times { queue_in_thread << "e_#{rand(100)}" }
 end
 
-#When all elements are dequeud, process method will wait for 10 secods before exit
+# When all elements are dequeud, process method will wait for 10 secods before exit
 queue.process do |message|
   puts "'#{message}'"
 end
